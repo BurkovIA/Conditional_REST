833e048d830212b8f4dfc6b39dcffaeefc23ed06
BurkovIA <burkov88@mail.ru>
2024-09-29 21:44:47 +0800

    Initial commit

---
+++ Conditional/src/main/java/com/example/Conditional/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.Conditional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
